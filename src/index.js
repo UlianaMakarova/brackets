74734933fe4e110608d778d64b470bcb534fc5b9
ulianamakarovaulianamakarovaexitgit config --globalgit config --global user.name ulianamakarova <ulianamakarova@gmail.com>
2023-02-11 17:35:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+
+  brasketDict = {};
+  bracketsConfig.forEach(pair=>{
+    brasketDict[pair[1]]=pair[0];
+    })
+  stack = [];
+  breakpoint = true;
+  str.split('').some(function(char){
+    if (Object.values(brasketDict).includes(char) && !(brasketDict[char] === char && stack.includes(char) )){
+      stack.push(char)
+    }
+    else if (Object.keys(brasketDict).includes(char)){
+      if (stack.length === 0 || stack[stack.length-1] !==brasketDict[char]){
+        breakpoint=false;
+        }
+      else {
+          stack.pop()
+        }}
+  })
+return stack.length === 0 && breakpoint;
+  }
